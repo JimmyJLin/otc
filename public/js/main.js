ecf8d0763c15cef03206e7f7312fbe226f0a1c9e
Jimmy Lin <usmcjimmy@gmail.com>
2017-06-12 23:51:35 -0400

    added footer

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,4 @@
 $(() => {
   console.log('script file loaded');
-
-  // Toggle Buttons - add class active if clicked and remove if click again
-  $('.button.toggle').on("click", () => {
-    if($(this).hasClass('selected')){
-      $(this).removeClass('selected').text('Vote')
-    } else {
-      $(this).addClass('selected').text('Voted')
-    }
-  });
+  // JQUERY GOES HERE!!
 });
