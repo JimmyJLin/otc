b3ce3530e1319ad31ebdcaacaa15e3e47b37a0ad
Jimmy Lin <usmcjimmy@gmail.com>
2017-06-21 15:53:58 -0400

    added 404 error page

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,19 @@
 $(() => {
   console.log('script file loaded');
   // JQUERY GOES HERE!!
+
+  // set focus when modal is opened
+  $('#modal-content').on('shown.bs.modal', () => {
+      $("#txtname").focus();
+  });
+
+  // show the modal onload
+  const showModal = () =>{
+    $('#modal-content').modal({
+      show: true
+    });
+  };
+
+setTimeout(showModal, 5000);
+
 });
--- routes/main.js
+++ routes/main.js
@@ -8,4 +8,8 @@ main.get('/', (req, res) => {
   res.render('pages/index');
 });
 
+main.get('/*', (req, res) => {
+  res.render('pages/404');
+});
+
 module.exports = main;
